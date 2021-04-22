a6400295d2c58b403a71ec7bbde034770d50e9ee
leleliu008 <leleliu008@gmail.com>
2021-04-23 01:26:17 +0800

    optimize

--- autogen.sh
+++ autogen.sh
@@ -266,7 +266,7 @@ EOF
                 __get_os_arch_from_getprop
                 ;;
             version)
-                case $(uname | tr A-Z a-z) in
+                case $(os type) in
                     freebsd) freebsd-version ;;
                     openbsd) uname -r ;;
                     netbsd)  uname -r ;;
@@ -283,7 +283,7 @@ EOF
             *)  echo "$1: not support item."; return 1
         esac
     else
-        echo "only support one item."; return 1
+        echo "os command only support one item."; return 1
     fi
 }
 
