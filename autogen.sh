be272e1388f319bd53ba7491ee5e4bfe2e0ec8a6
leleliu008 <leleliu008@gmail.com>
2021-04-27 12:02:27 +0800

    optimize

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# https://github.com/leleliu008/autogen.sh
+
 cd "$(dirname "$0")" || exit 1
 CURRENT_DIR="$PWD"
 
@@ -859,6 +861,8 @@ __available_package_manager_list() {
         echo brew
     fi
 
+    [ "$NATIVE_OS_TYPE" = 'darwin' ] && return
+
     if exists command apt ; then
         echo apt
     elif exists command apt-get ; then
@@ -947,7 +951,7 @@ __install_command_via_available_package_manager() {
                 warn "no package manager found."
                 return 1
             else
-                echo "Found $(list_length $AVAILABLE_PACKAGE_MANAGER_LIST) package manager : ${COLOR_GREEN}$AVAILABLE_PACKAGE_MANAGER_LIST${COLOR_OFF}"
+                echo "    Found $(list_length $AVAILABLE_PACKAGE_MANAGER_LIST) package manager : ${COLOR_GREEN}$AVAILABLE_PACKAGE_MANAGER_LIST${COLOR_OFF}"
             fi
         fi
         for pm in $AVAILABLE_PACKAGE_MANAGER_LIST
